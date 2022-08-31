6884da5ecfa31634a00f107467130ad016967902
mechztcc <dojathan@gmail.com>
2022-08-31 08:59:16 -0400

    feat: changes state component with pressed event

--- src/app/components/yes-or-not-button/yes-or-not-button.component.ts
+++ src/app/components/yes-or-not-button/yes-or-not-button.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-yes-or-not-button',
   templateUrl: './yes-or-not-button.component.html',
-  styleUrls: ['./yes-or-not-button.component.scss']
+  styleUrls: ['./yes-or-not-button.component.scss'],
 })
 export class YesOrNotButtonComponent implements OnInit {
+  value: string = 'no';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  activate(value: string) {
+    this.value = value;
   }
-
 }
